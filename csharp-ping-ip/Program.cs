63671a0da958b9e983a2e72e2ab5838cba848117
JiaHao Tan <jiahaotan@outlook.com>
2017-10-03 16:56:20 +0800

    First commit

--- csharp-ping-ip/Program.cs
+++ csharp-ping-ip/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,33 @@ namespace csharp_ping_ip
     {
         static void Main(string[] args)
         {
+            // Ping's the local machine.
+            Ping pingSender = new Ping();
+            //IPAddress address = IPAddress.Loopback;
+            IPAddress address = IPAddress.Parse("192.168.1.1");
+
+            for (int i = 0; i < 5; i++)
+            {
+                PingReply reply = pingSender.Send(address);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine(reply.Status);
+                    Console.WriteLine("Address: {0}", reply.Address.ToString());
+                    Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                    Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+                }
+                else
+                {
+                    Console.WriteLine(reply.Status);
+                }
+
+                System.Threading.Thread.Sleep(5000);
+            }
+            
+            Console.Read();
         }
     }
 }
